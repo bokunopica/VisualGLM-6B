6b714831cdfafbdb74ee16653cbe3ed512d9acf4
qq <qianq9720@163.com>
2023-07-29 15:00:55 +0800

    dataset process update

--- finetune_visualglm.py
+++ finetune_visualglm.py
@@ -18,6 +18,9 @@ class FineTuneVisualGLMModel(VisualGLMModel):
             # self.get_mixin("eva").model.glm_proj = replace_linear_with_lora(self.get_mixin("eva").model.glm_proj, LoraLinear, args.lora_rank)
         elif args.use_qlora:
             self.add_mixin("lora", LoraMixin(args.num_layers, args.lora_rank, layer_range=args.layer_range, qlora=True), reinit=True)
+        elif args.use_adapter:
+            # TODO add adapter way to finetune
+            pass
         self.args = args
         
     @classmethod
@@ -104,6 +107,7 @@ from model.blip2 import BlipImageEvalProcessor
 from torch.utils.data import Dataset
 import json
 from PIL import Image
+from tqdm import trange
 
 class FewShotDataset(Dataset):
     def __init__(self, path, processor, tokenizer, args):
@@ -153,11 +157,70 @@ class FewShotDataset(Dataset):
         }
 
 
+# def create_dataset_function(path, args):
+#     tokenizer = get_tokenizer(args)
+#     image_processor = BlipImageEvalProcessor(224)
+
+#     dataset = FewShotDataset(path, image_processor, tokenizer, args)
+#     return dataset
+
+
+class XrayDataset(Dataset):
+    def __init__(self, path, processor, tokenizer, args):
+        max_seq_length = args.max_source_length + args.max_target_length
+        with open(path, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+        prefix_path = '/'.join(path.split('/')[:-1])
+        self.images = []
+        self.input_ids = []
+        self.labels = []
+        for i in trange(len(data)):
+            item = data[i]
+            img_filename = item['img'].split('/')[-1]
+            image = processor(Image.open(f"{prefix_path}/{img_filename}").convert('RGB'))
+            input0 = tokenizer.encode("<img>", add_special_tokens=False)
+            input1 = [tokenizer.pad_token_id] * args.image_length
+            input2 = tokenizer.encode("</img>问："+item['prompt']+"\n答：", add_special_tokens=False)
+            a_ids = sum([input0, input1, input2], [])
+            b_ids = tokenizer.encode(text=item['label'], add_special_tokens=False)
+            if len(a_ids) > args.max_source_length - 1:
+                a_ids = a_ids[: args.max_source_length - 1]
+            if len(b_ids) > args.max_target_length - 2:
+                b_ids = b_ids[: args.max_target_length - 2]
+            pre_image = len(input0)
+            input_ids = tokenizer.build_inputs_with_special_tokens(a_ids, b_ids)
+
+            context_length = input_ids.index(tokenizer.bos_token_id)
+            mask_position = context_length - 1
+            labels = [-100] * context_length + input_ids[mask_position+1:]
+            
+            pad_len = max_seq_length - len(input_ids)
+            input_ids = input_ids + [tokenizer.pad_token_id] * pad_len
+            labels = labels + [tokenizer.pad_token_id] * pad_len
+            if args.ignore_pad_token_for_loss:
+                labels = [(l if l != tokenizer.pad_token_id else -100) for l in labels]
+            self.images.append(image)
+            self.input_ids.append(input_ids)
+            self.labels.append(labels)
+        self.pre_image = pre_image
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        return {
+            "image": self.images[idx],
+            "input_ids": self.input_ids[idx],
+            "labels": self.labels[idx],
+            "pre_image": self.pre_image
+        }
+
+
 def create_dataset_function(path, args):
     tokenizer = get_tokenizer(args)
     image_processor = BlipImageEvalProcessor(224)
 
-    dataset = FewShotDataset(path, image_processor, tokenizer, args)
+    dataset = XrayDataset(path, image_processor, tokenizer, args)
     return dataset
 
 
