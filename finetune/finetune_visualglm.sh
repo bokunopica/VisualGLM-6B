7087931bbc746a3515a64264352f01ab65d5e54b
qq <qianq9720@163.com>
2023-07-29 15:00:22 +0800

    lora bash fix

--- finetune/finetune_visualglm.sh
+++ finetune/finetune_visualglm.sh
@@ -13,20 +13,23 @@ MODEL_ARGS="--max_source_length 64 \
     --layer_range 0 14 \
     --pre_seq_len 4"
 
+OPTIONS_DEVICE="CUDA_VISIBLE_DEVICES=0,1,2"
 # OPTIONS_SAT="SAT_HOME=$1" #"SAT_HOME=/raid/dm/sat_models"
 OPTIONS_NCCL="NCCL_DEBUG=info NCCL_IB_DISABLE=0 NCCL_NET_GDR_LEVEL=2"
 HOST_FILE_PATH="hostfile"
 HOST_FILE_PATH="hostfile_single"
 
-train_data="./fewshot-data/dataset.json"
-eval_data="./fewshot-data/dataset.json"
+# train_data="./fewshot-data/dataset.json"
+# eval_data="./fewshot-data/dataset.json"
+train_data="/home/qianq/data/OpenI-zh/images/openi-zh-prompt.json"
+eval_data="/home/qianq/data/OpenI-zh/images/openi-zh-prompt.json"
 
 
 gpt_options=" \
        --experiment-name finetune-$MODEL_TYPE \
        --model-parallel-size ${MP_SIZE} \
        --mode finetune \
-       --train-iters 300 \
+       --train-iters 1000 \
        --resume-dataloader \
        $MODEL_ARGS \
        --train-data ${train_data} \
@@ -35,7 +38,7 @@ gpt_options=" \
        --lr-decay-style cosine \
        --warmup .02 \
        --checkpoint-activations \
-       --save-interval 300 \
+       --save-interval 1000 \
        --eval-interval 10000 \
        --save "./checkpoints" \
        --split 1 \
@@ -43,7 +46,7 @@ gpt_options=" \
        --eval-batch-size 8 \
        --zero-stage 1 \
        --lr 0.0001 \
-       --batch-size 4 \
+       --batch-size 8 \
        --skip-init \
        --fp16 \
        --use_lora
@@ -51,7 +54,7 @@ gpt_options=" \
 
               
 
-run_cmd="${OPTIONS_NCCL} ${OPTIONS_SAT} deepspeed --master_port 16666 --hostfile ${HOST_FILE_PATH} finetune_visualglm.py ${gpt_options}"
+run_cmd="${OPTIONS_DEVICE} ${OPTIONS_NCCL} ${OPTIONS_SAT} deepspeed --master_port 16666 --hostfile ${HOST_FILE_PATH} finetune_visualglm.py ${gpt_options}"
 echo ${run_cmd}
 eval ${run_cmd}
 
